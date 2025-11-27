db1daf333efd0240939b835980fa5b940b43fd67
jyothischandran <57825159+jyothischandran@users.noreply.github.com>
2025-11-27 18:22:08 +0000

    Deploying to gh-pages from @ jyothischandran/jyothischandran.github.io@6f0c3c34532f6951175657658a04f8e544fbc045 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jyothischandran/jyothischandran1597.github.io",
+    "data-repo": "jyothischandran/jyothischandran.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
